d62fdd5816934ccc478449c57f4d50cbf7bf80bf
Clayton Schnars <cschnars13@ufl.edu>
2019-12-31 13:59:40 -0500

    add material ui

--- src/StatSearchFields.js
+++ src/StatSearchFields.js
@@ -5,6 +5,8 @@ const StatSearchFields = () => {
     const { register, handleSubmit } = useForm()
 
     return (
+      <div>
+        <h1 style={{textAlign: 'center'}}>Stat Search</h1>
         <form onSubmit={handleSubmit(payload => console.log(payload))}>
             <input placeholder='Week' name='Week' ref={register} />
             <input placeholder='Team' name='Team' ref={register} />
@@ -12,6 +14,7 @@ const StatSearchFields = () => {
             <input placeholder='Position' name='Position' ref={register} />
             <input type='submit' />
         </form>
+      </div>
     )
 }
 
